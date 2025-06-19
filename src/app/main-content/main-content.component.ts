7de495872284f5d50c79aa433ae1ca7cf0c9256a
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2025-06-19 16:49:47 +0200

    styled the header

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeroComponent } from './hero/hero.component';
+import { FormularComponent } from './formular/formular.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [],
+  imports: [HeroComponent, FormularComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
