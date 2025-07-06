f92c9721b6c65ccb298f3ea82bcad219292cdf47
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2025-07-06 10:44:32 +0200

    added content to the privacy policy component

--- src/app/privacy-policy/privacy-policy.component.ts
+++ src/app/privacy-policy/privacy-policy.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
-export class PrivacyPolicyComponent {
+export class PrivacyPolicyComponent implements OnInit {
+  constructor(private router: Router) {}
 
+  ngOnInit() {
+    this.router.navigateByUrl('/privacy-policy').then(() => {
+      window.scrollTo(0, 0);
+    });
+  }
 }
