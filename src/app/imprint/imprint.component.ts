08e507d7329e8ae2229367df8f88bd7e9e5e3f1b
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2025-07-06 11:39:16 +0200

    added content to the imprint component

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
-export class ImprintComponent {
+export class ImprintComponent implements OnInit {
+  constructor(private router: Router) {}
 
+  ngOnInit() {
+    this.router.navigateByUrl('/imprint').then(() => {
+      window.scrollTo(0, 0);
+    });
+  }
 }
