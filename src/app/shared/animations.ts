0177a2c433b1aaef0d9d08d98f2221c15136bc30
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2025-07-17 19:39:42 +0200

    made small improvements

--- src/app/shared/animations.ts
+++ src/app/shared/animations.ts
@@ -2,6 +2,6 @@ import { animate, style, transition, trigger } from "@angular/animations";
 
 
 export const fadeIn = trigger('fadeIn', [
-    transition(':enter', [style({ opacity: '0' }), animate('500ms ease-in-out', style({ opacity: '1' }))]),
-    transition(':leave', [style({ opacity: '1' }), animate('500ms ease-in-out', style({ opacity: '0' }))]),
+    transition(':enter', [style({ opacity: '0' }), animate('300ms ease-in-out', style({ opacity: '1' }))]),
+    transition(':leave', [style({ opacity: '1' }), animate('300ms ease-in-out', style({ opacity: '0' }))]),
 ]);
