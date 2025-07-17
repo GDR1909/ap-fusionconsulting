5295dd510ac6c29ece3bd0766208ab77c7a53199
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2025-07-17 19:27:02 +0200

    added the animate on scroll library to the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import { isPlatformBrowser } from '@angular/common';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ap-fusionconsulting';
+
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  // ngOnInit() {
+  //   AOS.init({
+  //     duration: 800,
+  //     easing: 'ease-in-out',
+  //     once: true,
+  //   });
+  // }
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      import('aos').then((AOS) => {
+        AOS.init({
+          duration: 1500,
+          easing: 'ease-in-out',
+          once: true,
+        });
+      });
+    }
+  }
 }
